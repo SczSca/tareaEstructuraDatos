c6a7f445cf991624f6c185f10a0c6b182a3a9670
SczSca <90069772+SczSca@users.noreply.github.com>
2022-09-30 11:00:42 -0500

    BIG ERROR FIXED
    STILL AN ERROR WITH LAST VALUE OF ARR2

--- binarySTest.js
+++ binarySTest.js
@@ -4,7 +4,7 @@ let b = 16;
 function rankForBSearch(key, arr){
     let aux = [];
     let posicion = [];
-    let arrAux = arr;
+    let arrAux = [...arr]; // == let arrAux = arr; pero sin modificar al original
     for(let i= 0; i< key.length;i++){
         let lo = 0;
         let hi = arrAux.length - 1;
@@ -20,20 +20,19 @@ function rankForBSearch(key, arr){
             else hi --;
 
         }
-        posicion[i] = mid + 1;
-        for(let j = arrAux.length - 1; j >= mid + 1; j--){
-            arrAux[j + 1] = arrAux[j]
+        posicion[i] = mid ;
+        for(let j = arrAux.length - 1; j >= mid ; j--){
+            arrAux[j + 1] = arrAux[j];
         }
-        arrAux[mid + 1] = key[i];
+        arrAux[mid] = key[i];
     }
     aux = [arrAux,posicion];
    return aux;
 }
-// vec3 = rankForBSearch(vec2,vec1); si genero esto antes de querer imprimir vec1, el vec1 se iguala a vec3 Y NO ENTIENDO POR QUE
-// console.log(vec3);
+vec3 = rankForBSearch(vec2,vec1); 
 console.log(vec1);
 console.log(vec2);
 vec3 = rankForBSearch(vec2,vec1)[0];
-// pos = rankForBSearch(vec2, vec1)[1]; si genero esto, el vec3 se le agregan mas valores Y NO ENTIENDO POR QUE
-console.log(vec3);
-// console.log(pos);
+pos = rankForBSearch(vec2, vec1)[1]; 
+console.log(vec3); //output [1, 1, 1, 2, 2, 3, 4, 5, 6, 6, 7, 7, 8, 8, 8, 9, 9, 9, 9, 9, 9, 10, 11, 12, 13, 14, 15, 15, 16, 15] ERROR EN ULTIMO VALOR DE ARR2 SE INTRODUCE EN PENULTIMA
+console.log(pos); //[3, 7, 8, 10, 12, 15, 15, 15, 15, 15, 21, 22, 23, 24, 25, 26, 26, 28] <- indice 28 es donde se introduce el valor 16
